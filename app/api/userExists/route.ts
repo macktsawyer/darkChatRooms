a1bd35eb419e4d932f9c1f8e00c13295e9b60465
macktsawyer <macktsawyer@yahoo.com>
2025-01-29 10:52:39 -0500

    Same fix as before

--- app/api/userExists/route.ts
+++ app/api/userExists/route.ts
@@ -2,13 +2,17 @@ import { NextResponse, NextRequest } from "next/server";
 import { connectToDatabase } from '../../../utils/mongodb';
 import User from "../../../models/User.js";
 
-export async function POST(req: NextRequest, res: NextResponse) {
+export async function POST(req: NextRequest) {
     try {
         await connectToDatabase();
         const { email } = await req.json();
         const user = await User.findOne({ email }).select("id");
-        return NextResponse.json({user});
+        return NextResponse.json({ user });
     } catch (error) {
         console.log(error);
+        return NextResponse.json({
+            message: "An error occurred while checking if the user exists.",
+            status: 500,
+        });
     }
 }
