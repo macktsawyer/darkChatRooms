e08f2905c124af7fb2e36bacc71245d6c75bde9c
macktsawyer <macktsawyer@yahoo.com>
2025-01-29 10:50:08 -0500

    Similar treatment as login fix

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -4,16 +4,18 @@ import User from "../../../models/User.js";
 import { connectToDatabase } from "../../../utils/mongodb.js";
 import bcrypt from 'bcryptjs';
 
-export async function POST(req: NextRequest, res: NextResponse) {
+export async function POST(req: NextRequest) {
     try {
         const body = await req.json();
-        const { username, email, password } : 
-                { username: string; email: string; password: string; } = body;
+        const { username, email, password }: 
+                { username: string; email: string; password: string } = body;
 
         try {
             const hashedPassword = await bcrypt.hash(password, 10);
-            console.log(hashedPassword)
+            console.log(hashedPassword);
+
             await connectToDatabase();
+
             const newUser = new User({
                 id: uuidv4(),
                 username: username,
@@ -21,8 +23,9 @@ export async function POST(req: NextRequest, res: NextResponse) {
                 password: hashedPassword,
                 creationDate: Date.now(),
                 defaultMessageColor: "slate"
-            })
-            await newUser.save()
+            });
+
+            await newUser.save();
         } catch (error) {
             if (process.env.APP_INSTANCE === "DEV") {
                 console.log("ErrCode 1003: Error when attempting to create a new user: " + error);
@@ -30,19 +33,20 @@ export async function POST(req: NextRequest, res: NextResponse) {
                 console.log("ErrCode 1003");
             }
             return NextResponse.json({
-                message: "ErrCode 1003: An error occured while registering the user.",
+                message: "ErrCode 1003: An error occurred while registering the user.",
                 status: 500,
-            })
+            });
         }
 
         return NextResponse.json({
             message: "Success",
-            status: 201
-        })
+            status: 201,
+        });
+
     } catch (error) {
         return NextResponse.json({
-            message: "An error occured while registering the user.",
+            message: "An error occurred while registering the user.",
             status: 500,
-        })
+        });
     }
 }
