60d83200330fd6d121732f7f7cb55a06515b2879
macktsawyer <macktsawyer@yahoo.com>
2025-01-29 10:55:43 -0500

    Removing unused import

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState, useRef, FormEvent } from "react";
-import { authOptions } from './api/auth/[...nextauth]/route';
+
 import { useSession } from "next-auth/react";
 import { redirect } from 'next/navigation';
 import classNames from 'classnames';
